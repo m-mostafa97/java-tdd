e0f6b5f6dfdbe601bcfd2bf470a49df491dcc6c9
Mohamed Mostafa <ositrade@momostafa.local>
2025-04-19 03:01:23 +0200

    fix create task

--- src/main/java/com/khalifa/tdd/controller/TaskController.java
+++ src/main/java/com/khalifa/tdd/controller/TaskController.java
@@ -5,13 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.khalifa.tdd.model.Task;
 import com.khalifa.tdd.service.TaskService;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@Slf4j
 public class TaskController {
     @Autowired
     TaskService taskService;
@@ -21,4 +27,9 @@ public class TaskController {
         return taskService.getAllTasks();
     }
     
+    @PostMapping("/tasks")
+    public Task saveTask(@RequestBody Task task) {
+        log.info("Saving task: {}", task);
+        return taskService.saveTask(task);
+    }
 }
--- src/main/java/com/khalifa/tdd/service/TaskService.java
+++ src/main/java/com/khalifa/tdd/service/TaskService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.khalifa.tdd.model.Task;
 import com.khalifa.tdd.repository.TaskRepository;
 
+@Service
 public class TaskService {
     @Autowired
     TaskRepository taskRepository;
